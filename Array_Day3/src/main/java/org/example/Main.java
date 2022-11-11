d31fca38654b9eb4146aaa131f8277ebb969480b
ratnadipbharde <ratnadipbharde@gmail.com>
2022-11-11 17:06:31 +0530

    Added Prog 3 Find largest Element in Array Solution

--- Array_Day3/src/main/java/org/example/Main.java
+++ Array_Day3/src/main/java/org/example/Main.java
@@ -7,9 +7,14 @@ public class Main {
         obj1.print();
         System.out.println("------------------------------");
 
- 	//prog 2) Java Program to find the frequency of each element in the array
+ 	    //prog 2) Java Program to find the frequency of each element in the array
         FrequencyofElement obj2=new FrequencyofElement();
         obj2.frequency();
+        System.out.println("------------------------------");
+
+        //prog 3) Java Program to print the largest element in an array
+        largestElement obj3=new largestElement();
+        obj3.largest();
         System.out.println("------------------------------");
     }
 }
